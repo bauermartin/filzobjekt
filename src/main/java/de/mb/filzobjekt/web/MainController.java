7775b60dd5679f7bc48472eeed94a4f67b7176f7
Martin Bauer <martin.bauer@pentos.com>
2015-10-23 17:23:05 +0200

    getting started with thymeleaf

--- src/main/java/de/mb/filzobjekt/web/MainController.java
+++ src/main/java/de/mb/filzobjekt/web/MainController.java
@@ -3,6 +3,7 @@ package de.mb.filzobjekt.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,10 +11,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class MainController {
-	
-	@RequestMapping(value="/test")
+
+	private String currentPage = "";
+
+	@RequestMapping("/")
+	private String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/test")
 	private ResponseEntity<String> test() {
 		return new ResponseEntity<>("test", HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "/home")
+	private String home() {
+		currentPage = "home";
+		return "index";
+	}
+	@RequestMapping(value = "/shoes")
+	private String shoes() {
+		currentPage = "shoes";
+		return "index";
+	}
+
+	@ModelAttribute("ishome")
+	private boolean ishome() {
+		return "home".equals(currentPage);
+	}
+	@ModelAttribute("isshoes")
+	private boolean isshoes() {
+		return "shoes".equals(currentPage);
+	}
 }
