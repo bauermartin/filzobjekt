49bb53298274c06bb543ecc88f057b5ddbd29052
Martin Bauer <martin.bauer@pentos.com>
2016-01-08 17:00:18 +0100

    bootstrap getting started

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -8,16 +8,15 @@ if (typeof de.filzobjekt == 'undefined') {
 	de.filzobjekt = {};
 }
 /**
- *
+ * 
  */
-(function ($) {
+(function($) {
 	init();
 
-	de.filzobjekt.render = function () {
-		de.filzobjekt.nav.render();
+	de.filzobjekt.render = function() {
 		de.filzobjekt.mainmodel.render();
 	};
-	de.filzobjekt.text = function (ev, type) {
+	de.filzobjekt.text = function(ev, type) {
 
 		console.log(ev);
 		de.filzobjekt.mainmodel.text(type);
@@ -26,25 +25,25 @@ if (typeof de.filzobjekt == 'undefined') {
 })(jQuery);
 
 /**
- *
+ * 
  */
 function init() {
 	de.filzobjekt.mainmodel = new MainModel();
 	de.filzobjekt.nav = new NavModel();
 }
 /**
- *
+ * 
  * @constructor
  */
 function MainModel() {
 	var self = this;
 	self.text_dialog = null;
 	self.content = null;
-	self.text = function (type) {
+	self.text = function(type) {
 
 	};
-	self.render = function () {
-		$('.text_dialog').click(function () {
+	self.render = function() {
+		$('.text_dialog').click(function() {
 			var self = $(this);
 			console.log(self.attr('id'));
 			renderDialog(self);
@@ -52,19 +51,23 @@ function MainModel() {
 	};
 	function renderDialog(elem) {
 		if (self.text_dialog === null) {
-			self.content = $('<textarea>',
-				{width : '100%', height : '100%'}).val(elem.html());
-			var dialog = $('<div>', {width : 1000, height : 300}).append(self.content);
+			self.content = $('<textarea>', {
+				width : '100%',
+				height : '100%'
+			}).val(elem.html());
+			var dialog = $('<div>', {
+				width : 1000,
+				height : 300
+			}).append(self.content);
 			$('body').append(dialog);
 			self.text_dialog = $(dialog).dialog({
-					title : "Text Editor",
-					resizable : true,
-					modal : true,
-					draggable : false,
-					width : 1000,
-					height : 300
-				}
-			);
+				title : "Text Editor",
+				resizable : true,
+				modal : true,
+				draggable : false,
+				width : 1000,
+				height : 300
+			});
 		} else {
 			self.content.val(elem.html());
 			self.text_dialog.dialog().open();
@@ -76,7 +79,7 @@ function cleanText(text) {
 	var text_lines = text.split("\n");
 	var format_text = "";
 	var first = true;
-	$.each(text_lines, function () {
+	$.each(text_lines, function() {
 		var clean_text = this.trim();
 		if (clean_text !== null && clean_text !== "") {
 			if (!first) {
@@ -90,24 +93,10 @@ function cleanText(text) {
 }
 
 /**
- *
+ * 
  * @constructor
  */
 function NavModel() {
 	var self = this;
-	self.render = function () {
-		self.home_button = $('button[name=home]').button();
-
-		self.shoes_button = $('button[name=shoes]').button();
-
-		self.handbag_button = $('button[name=handbag]').button();
-
-		self.object_button = $('button[name=object]').button();
-		self.seat_pad_button = $('button[name=seat_pad]').button();
-		self.scarf_button = $('button[name=scarf]').button();
-		self.gabi_button = $('button[name=gabi]').button();
-		self.impressum_button = $('button[name=impressum]').button();
-		self.kontakt_button = $('button[name=kontakt]').button();
-	};
 
 }
