eb68eea9852f18ac68f04438da12a0ed05a3860f
Martin Bauer <martin.bauer@pentos.com>
2016-01-09 13:19:37 +0100

    ckeditor

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -16,12 +16,6 @@ if (typeof de.filzobjekt == 'undefined') {
 	de.filzobjekt.render = function() {
 		de.filzobjekt.mainmodel.render();
 	};
-	de.filzobjekt.text = function(ev, type) {
-
-		console.log(ev);
-		de.filzobjekt.mainmodel.text(type);
-	};
-
 })(jQuery);
 
 /**
@@ -29,7 +23,6 @@ if (typeof de.filzobjekt == 'undefined') {
  */
 function init() {
 	de.filzobjekt.mainmodel = new MainModel();
-	de.filzobjekt.nav = new NavModel();
 }
 /**
  * 
@@ -37,68 +30,33 @@ function init() {
  */
 function MainModel() {
 	var self = this;
-	self.text_dialog = null;
-	self.content = null;
-	self.text = function(type) {
-
-	};
 	self.render = function() {
+		var editor = CKEDITOR.instances["editor"];
+
+		var close_ev = false;
+		$('#dialog_close1').click(function() {
+			close_ev = true;
+		});
+		$('#dialog_close2').click(function() {
+			close_ev = true;
+		});
+		var orig_field = null;
 		$('.text_dialog').click(function() {
-			// var self = $(this);
-			// console.log(self.attr('id'));
-			// renderDialog(self);
-			$('#modalContent');
+			close_ev = false;
+			orig_field = $(this);
+			var editor_field = $('#editor');
+			if (editor) {
+				editor.destroy(true);
+			}
+			editor_field.text(orig_field.html());
+			editor = CKEDITOR.replace(editor_field.get(0));
 			$('#dialog').modal('toggle');
 		});
-	};
-	function renderDialog(elem) {
-		if (self.text_dialog === null) {
-			self.content = $('<textarea>', {
-				width : '100%',
-				height : '100%'
-			}).val(elem.html());
-			var dialog = $('<div>', {
-				width : 1000,
-				height : 300
-			}).append(self.content);
-			$('body').append(dialog);
-			self.text_dialog = $(dialog).dialog({
-				title : "Text Editor",
-				resizable : true,
-				modal : true,
-				draggable : false,
-				width : 1000,
-				height : 300
-			});
-		} else {
-			self.content.val(elem.html());
-			self.text_dialog.dialog().open();
-		}
-	}
-}
 
-function cleanText(text) {
-	var text_lines = text.split("\n");
-	var format_text = "";
-	var first = true;
-	$.each(text_lines, function() {
-		var clean_text = this.trim();
-		if (clean_text !== null && clean_text !== "") {
-			if (!first) {
-				format_text += "\n";
+		$('#dialog').on('hidden.bs.modal', function(e) {
+			if (!close_ev) {
+				orig_field.html(editor.getData());
 			}
-			first = false;
-			format_text += clean_text;
-		}
-	});
-	return format_text;
-}
-
-/**
- * 
- * @constructor
- */
-function NavModel() {
-	var self = this;
-
+		});
+	};
 }
